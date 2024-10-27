28bc576b65ebb86dc15beaacb90bb7a68952c2a0
stevecalla <callasteven@gmail.com>
2024-10-27 17:10:33 -0600

    Update OS path to include linux

--- utilities/determineOSPath.js
+++ utilities/determineOSPath.js
@@ -1,11 +1,12 @@
-const csv_export_path = `C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/data/`;
+const csv_export_path_linux = `/home/steve-calla/development/usat/data/`;
 const csv_export_path_mac = `/Users/teamkwsc/development/usat/data/`;
+const csv_export_path_windows = `C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/data/`;
 
 async function determineOSPath() {
     // Determine the CSV export path based on the OS
     const isMac = process.platform === 'darwin'; // macOS
-    // const isWindows = process.platform === 'win32'; // Windows
-    const os_path = isMac ? csv_export_path_mac : csv_export_path;
+    const isLinux = process.platform === 'linux'; // Linux
+    const os_path = isMac ? csv_export_path_mac : (isLinux ? csv_export_path_linux : csv_export_path_windows);
     return os_path;
 }
 
