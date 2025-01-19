569540c75f3d0cb2dacef7e9f2db69aea63ad20a
stevecalla <callasteven@gmail.com>
2025-01-19 10:58:33 -0700

    Adjust csv file size to 5000

--- src/sales_data/step_1_get_sales_data.js
+++ src/sales_data/step_1_get_sales_data.js
@@ -419,7 +419,7 @@ async function execute_get_sales_data() {
     let results;
     let offset = 0;
     const retrieval_batch_size = 30000; // Retrieve 30,000 records at a time
-    const write_batch_size = 1000; // Write 1,000 records at a time
+    const write_batch_size = 5000; // Write 1,000 records at a time
     const start_year = 2024; // Default = 2025
     const membershipPeriodEnds = '2008-01-01';
     const period_interval = 6; // create date periods for 6 month durations; options in include 1 month and 3 months
