7bf3321d2a99b4483675af614ccf5b948c757d7f
stevecalla <callasteven@gmail.com>
2025-01-20 22:42:51 -0700

    Set query result to null to allow for garbage collection

--- src/google_cloud/step_1_retrieve_data_process.js
+++ src/google_cloud/step_1_retrieve_data_process.js
@@ -205,13 +205,14 @@ async function execute_retrieve_data(options) {
             pool = await create_local_db_connection(await local_usat_sales_db_config());
 
             let results = await execute_get_data(pool, fileName, query);
-            // console.table(results[0]);         
-            // console.log(results[0]);
 
             // STEP 3a SAVE DATA TO CSV FILE
             console.log(`STEP 3a SAVE ${fileName} TO CSV FILE`);
 
             await export_results_to_csv_fast_csv(results, fileName, i); 
+
+            // This removes references, making the memory eligible for garbage collection.
+            results = null;
 
             stopTimer(`${i}_get_data`);  
         }
