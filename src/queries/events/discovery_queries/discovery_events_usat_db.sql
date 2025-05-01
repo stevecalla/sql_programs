19b0be017e28d7eedca57f8304035fa2e05001fa
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-05-01 17:38:11 -0600

    Add query to check missing events

--- src/queries/events/discovery_queries/discovery_events_usat_db.sql
+++ src/queries/events/discovery_queries/discovery_events_usat_db.sql
@@ -1,6 +1,12 @@
 USE vapor;
 
-SELECT * FROM events AS e WHERE e.sanctioning_event_id = '307440'LIMIT 10;
+SELECT * FROM events AS e WHERE e.sanctioning_event_id = '307440' LIMIT 10;
+
+SELECT * FROM events AS e WHERE e.sanctioning_event_id IN (307440, 350276) LIMIT 10;
+
+SELECT sanctioning_event_id, name, created_at, deleted_at, status FROM events AS e WHERE e.sanctioning_event_id IN ( 350276, 350168, 350272, 350260, 350265, 350270, 350278, 
+350286) LIMIT 10;
+
 SELECT COUNT(*), COUNT(DISTINCT id), COUNT(DISTINCT sanctioning_event_id) FROM events;
 SELECT status, COUNT(status) FROM events GROUP BY 1;
 SELECT YEAR(starts), COUNT(*) FROM events GROUP BY 1;
