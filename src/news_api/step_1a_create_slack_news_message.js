48116ad69847527441e24650285daf06af4d169c
stevecalla <callasteven@gmail.com>
2025-06-17 20:52:23 -0600

    Adjust news title length to 55 from 60

--- src/news_api/step_1a_create_slack_news_message.js
+++ src/news_api/step_1a_create_slack_news_message.js
@@ -25,7 +25,7 @@ function padString(str, length) {
 async function format_markdown_table_news_data(news_data) {
     const headers = ['Date', 'Title (hover & click link)'];
     const date_width = 17;
-    const title_width = 60;
+    const title_width = 55;
     const colWidths = [date_width, title_width];
 
     // Format header row
