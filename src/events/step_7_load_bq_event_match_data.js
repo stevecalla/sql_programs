1f2e2c87154833bdcbbc8969877e2d80724f376b
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-07-08 12:06:56 -0600

    Add step 7 code

--- src/events/step_7_load_bq_event_match_data.js
+++ src/events/step_7_load_bq_event_match_data.js
@@ -4,13 +4,13 @@ dotenv.config({ path: "../../.env" });
 const { logPM2MemoryUsage } = require('../../utilities/pm2_scripts/pm2_log_memory_usage');
 const { runTimer, stopTimer } = require('../../utilities/timer');
 
-const { query_participation_profile} = require('../google_cloud/queries/query_participation_profile');
-const { participation_profile_schema } = require('../google_cloud/schemas/schema_participation_profile');
+const { query_event_match_data } = require('../google_cloud/queries/query_event_match_data');
+const { event_data_metrics_yoy_match_schema } = require('../google_cloud/schemas/schema_event_match_data');
 
 const { execute_load_data_to_bigquery } = require('../google_cloud/step_0_load_main_job');
 
 // EXECUTE LOAD BIQ QUERY
-async function execute_load_big_query_participation_profile_metrics() {
+async function execute_load_big_query_event_match_data() {
     runTimer(`load_bigquery`);
 
     const app_name = "usat_sales";
@@ -18,19 +18,19 @@ async function execute_load_big_query_participation_profile_metrics() {
 
     const options = [
         {
-            fileName: 'participation_profile_data',
-            query: (retrieval_batch_size, offset) => query_participation_profile(retrieval_batch_size, offset),
-            tableId: 'participation_profile_data', // table name
+            fileName: 'event_data_metrics_yoy_match',
+            query: (retrieval_batch_size, offset) => query_event_match_data(retrieval_batch_size, offset),
+            tableId: 'event_data_metrics_yoy_match', // table name
             
-            // fileName: 'participation_profile_data_v2',
-            // tableId: "participation_profile_data_v2",
+            // fileName: 'event_data_metrics_yoy_matcha_v2',
+            // tableId: "event_data_metrics_yoy_match_v2",
         }
     ];
     
     const directoryName = `usat_bigquery_${options[0].fileName}`;
     const datasetId = "membership_reporting"; // database name
     const bucketName = 'membership-reporting';
-    const schema = participation_profile_schema;
+    const schema = event_data_metrics_yoy_match_schema;
     
     await execute_load_data_to_bigquery(options, datasetId, bucketName, schema, directoryName);
 
@@ -44,7 +44,7 @@ async function execute_load_big_query_participation_profile_metrics() {
     return true; // placeholder to return to ensure success msg
 }
 
-// execute_load_big_query_participation_race_profile_metrics();
+// execute_load_big_query_event_match_data();
 // (async () => {
 //     try {
 //         console.log('\nStarting data load.');
@@ -55,5 +55,5 @@ async function execute_load_big_query_participation_profile_metrics() {
 // })();
 
 module.exports = {
-    execute_load_big_query_participation_profile_metrics,
+    execute_load_big_query_event_match_data,
 }
