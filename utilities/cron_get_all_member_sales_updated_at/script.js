0fde0381f5ef5dfcb65bb2c1bfdd5efe73573272
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-08-10 08:34:24 -0600

    Add a third update mode... full, partial & updated at

---
+++ utilities/cron_get_all_member_sales_updated_at/script.js
@@ -0,0 +1,18 @@
+console.log(`\nHello - RUN ALL MEMBERSHIP SALES JOB - PARTIAL UPDATE`);
+console.log("Current Date and Time:", new Date().toLocaleString());
+
+fetch('http://localhost:8003/scheduled-all-sales-updated-at')
+// fetch('http://localhost:8003/scheduled-all-sales-test')
+    .then(response => {
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(data => {
+        console.log('Response from /scheduled-all-usat-sales_partial_update:', data);
+    })
+    .catch(error => {
+        console.error('Error with request /scheduled-all-usat-sales_partial_update:', error.message);
+    });
