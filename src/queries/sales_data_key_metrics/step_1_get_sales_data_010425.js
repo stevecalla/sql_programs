b7b4df8dc7a846a96b2996323b4d25abd8f021c3
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-08-30 11:51:29 -0600

    Add from statement

--- src/queries/sales_data_key_metrics/step_1_get_sales_data_010425.js
+++ src/queries/sales_data_key_metrics/step_1_get_sales_data_010425.js
@@ -1,14 +1,13 @@
 // SOURCE?
 // C:\Users\calla\development\usat\sql_code\6_create_key_stats\key_stats_query_cte_create_table_100524.sql
 
-function step_1_member_minimum_first_created_at_dates() {
+function step_1_member_minimum_first_created_at_dates(FROM_STATEMENT) {
     return `
-        -- STEP #1 = CREATE MINIMUM FIRST CREATED AT DATES TABLE -- TODO: DONE 90 SECS
+        -- STEP #1 = CREATE MINIMUM FIRST CREATED AT DATES TABLE
         DROP TABLE IF EXISTS step_1_member_minimum_first_created_at_dates;
 
             CREATE TABLE step_1_member_minimum_first_created_at_dates AS
                 SELECT 
-                    -- member_number_members_sa,
                     id_profiles,
 
                     MIN(created_at_members) AS first_created_at_members,
@@ -20,9 +19,9 @@ function step_1_member_minimum_first_created_at_dates() {
 
                     YEAR(MIN(purchased_on_adjusted_mp)) AS first_purchased_on_year_adjusted_mp
 
-                FROM all_membership_sales_data_2015_left
+                -- FROM all_membership_sales_data_2015_left
+                ${FROM_STATEMENT}
                 GROUP BY id_profiles
-                -- GROUP BY member_number_members_sa
             ;
 
             ALTER TABLE step_1_member_minimum_first_created_at_dates ADD INDEX (id_profiles);     
