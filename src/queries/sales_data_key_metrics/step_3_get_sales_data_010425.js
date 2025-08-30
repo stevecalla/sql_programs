a78cf56bc4d341d82d4880eb2f40637cb0deae49
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-08-30 11:53:16 -0600

    Add from statement

--- src/queries/sales_data_key_metrics/step_3_get_sales_data_010425.js
+++ src/queries/sales_data_key_metrics/step_3_get_sales_data_010425.js
@@ -1,21 +1,20 @@
 // SOURCE?
 // C:\Users\calla\development\usat\sql_code\6_create_key_stats\key_stats_query_cte_create_table_100524.sql
 
-function step_3_member_total_life_time_purchases() {
+function step_3_member_total_life_time_purchases(FROM_STATEMENT) {
     return `
-        -- STEP #3 = CREATE TOTAL LIFETIME PURCHASES TABLE -- TODO: DONE 61 secs
+        -- STEP #3 = CREATE TOTAL LIFETIME PURCHASES TABLE
         DROP TABLE IF EXISTS step_3_member_total_life_time_purchases;
 
-        CREATE TABLE step_3_member_total_life_time_purchases AS
-            SELECT
-                -- member_number_members_sa,
-                id_profiles,
-                COUNT(*) AS member_lifetime_purchases -- total lifetime purchases due to group by
+            CREATE TABLE step_3_member_total_life_time_purchases AS
+                SELECT
+                    id_profiles,
+                    COUNT(*) AS member_lifetime_purchases -- total lifetime purchases due to group by
 
-            FROM all_membership_sales_data_2015_left
-            -- GROUP BY member_number_members_sa
-            GROUP BY id_profiles
-        ;
+                -- FROM all_membership_sales_data_2015_left
+                ${FROM_STATEMENT}
+                GROUP BY id_profiles
+            ;
 
         ALTER TABLE step_3_member_total_life_time_purchases     
             ADD INDEX (id_profiles);
