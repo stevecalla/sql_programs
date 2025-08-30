e9f74d2cbc19f9654bc6cd430bb3be6c918840df
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-08-30 11:54:40 -0600

    Add from statement

--- src/queries/sales_data_key_metrics/step_5_get_sales_data_010425.js
+++ src/queries/sales_data_key_metrics/step_5_get_sales_data_010425.js
@@ -1,28 +1,37 @@
 // SOURCE?
 // C:\Users\calla\development\usat\sql_code\6_create_key_stats\key_stats_query_cte_create_table_100524.sql
 
-function step_5_member_age_at_sale_date() {
+function step_5_member_age_at_sale_date(FROM_STATEMENT) {
     return `
         -- STEP #5 = CREATE MEMBER AGE AT SALE DATE TABLE
         DROP TABLE IF EXISTS step_5_member_age_at_sale_date;
 
-        CREATE TABLE step_5_member_age_at_sale_date AS
-            SELECT
-                am.id_profiles,
-                -- am.member_number_members_sa,
-                am.id_membership_periods_sa,
-                
-                (
-                    YEAR(MAX(purchased_on_adjusted_mp)) - YEAR(MAX(am.date_of_birth_profiles))) - 
-                    (DATE_FORMAT(MAX(am.purchased_on_adjusted_mp), '%m%d') 
-                    < DATE_FORMAT(MAX(am.date_of_birth_profiles), '%m%d')
-                )   
-                AS age_as_of_sale_date -- create age of of sale date
+            CREATE TABLE step_5_member_age_at_sale_date AS
+                WITH min_dates AS (
+                    SELECT
+                        am.id_membership_periods_sa,
 
-            FROM all_membership_sales_data_2015_left as am
-            -- GROUP BY am.member_number_members_sa, am.id_membership_periods_sa 
-            GROUP BY am.id_profiles, am.id_membership_periods_sa 
-        ;
+                        MIN(am.purchased_on_adjusted_mp) AS sale_date,
+
+                        -- DOB should be constant per member; MIN/MAX equivalent if duplicates exist
+                        MIN(am.date_of_birth_profiles)    AS dob
+
+                    ${FROM_STATEMENT} AS am
+
+                    -- WHERE 1 = 1 
+                        -- AND am.id_membership_periods_sa     IS NOT NULL
+                        -- AND am.purchased_on_adjusted_mp     IS NOT NULL
+                        -- AND am.date_of_birth_profiles       IS NOT NULL
+
+                    GROUP BY am.id_membership_periods_sa
+                    
+                    )
+                    
+                    SELECT
+                        id_membership_periods_sa,
+                        TIMESTAMPDIFF(YEAR, dob, sale_date) AS age_as_of_sale_date
+                    FROM min_dates
+            ;
 
         ALTER TABLE step_5_member_age_at_sale_date              
             ADD INDEX (id_membership_periods_sa);
