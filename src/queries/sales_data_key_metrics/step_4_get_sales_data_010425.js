b0cf24f2d28765dc2ec7e810df8a189f4e33a495
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-08-30 11:53:44 -0600

    Add from statement

--- src/queries/sales_data_key_metrics/step_4_get_sales_data_010425.js
+++ src/queries/sales_data_key_metrics/step_4_get_sales_data_010425.js
@@ -1,23 +1,29 @@
 // SOURCE?
 // C:\Users\calla\development\usat\sql_code\6_create_key_stats\key_stats_query_cte_create_table_100524.sql
 
-function step_4_member_age_dimensions() {
+function step_4_member_age_dimensions(FROM_STATEMENT) {
     return `
-        -- STEP #4 = CREATE AGE NOW TABLE -- TODO: done 92
+        -- STEP #4 = CREATE AGE NOW TABLE
         DROP TABLE IF EXISTS step_4_member_age_dimensions;
 
-        CREATE TABLE step_4_member_age_dimensions AS
-            SELECT
-                -- member_number_members_sa,
-                id_profiles,
+            CREATE TABLE step_4_member_age_dimensions AS
+                SELECT
+                    id_profiles,
 
-                (YEAR(CURDATE()) - YEAR(date_of_birth_profiles)) - (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(date_of_birth_profiles, '%m%d')) AS age_now, -- create age as of now,
-                MIN(date_of_birth_profiles) AS date_of_birth_profiles
+                    (
+                        YEAR(CURDATE()) 
+                        - YEAR(MIN(date_of_birth_profiles))) 
+                        - (DATE_FORMAT(CURDATE(), '%m%d') 
+                        < DATE_FORMAT(MIN(date_of_birth_profiles), '%m%d')
+                        
+                    ) AS age_now, -- create age as of now,
 
-            FROM all_membership_sales_data_2015_left
-            -- GROUP BY member_number_members_sa, 2
-            GROUP BY id_profiles, 2
-        ;
+                    MIN(date_of_birth_profiles) AS date_of_birth_profiles
+
+                -- FROM all_membership_sales_data_2015_left
+                ${FROM_STATEMENT}
+                GROUP BY id_profiles
+            ;
 
         ALTER TABLE step_4_member_age_dimensions                
             ADD INDEX (id_profiles);
