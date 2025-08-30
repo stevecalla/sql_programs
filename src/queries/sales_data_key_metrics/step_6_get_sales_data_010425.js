1bff336749220c771c1e448817721ba40376d0b8
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-08-30 11:56:01 -0600

    Add from statement

--- src/queries/sales_data_key_metrics/step_6_get_sales_data_010425.js
+++ src/queries/sales_data_key_metrics/step_6_get_sales_data_010425.js
@@ -1,22 +1,24 @@
 // SOURCE?
 // C:\Users\calla\development\usat\sql_code\6_create_key_stats\key_stats_query_cte_create_table_100524.sql
 
-function step_6_membership_period_stats() {
+function step_6_membership_period_stats(FROM_STATEMENT) {
     return `
-        -- STEP #6 = CREATE MEMBERSHIP PERIOD STATS TABLE -- TODO: 
+        -- STEP #6 = CREATE MEMBERSHIP PERIOD STATS TABLE 
         DROP TABLE IF EXISTS step_6_membership_period_stats;
 
-        CREATE TABLE step_6_membership_period_stats AS
-            SELECT
-                id_membership_periods_sa,
-                actual_membership_fee_6_rule_sa,
-                    
-                COUNT(id_membership_periods_sa) AS sales_units,
-                SUM(actual_membership_fee_6_sa) AS sales_revenue
+            CREATE TABLE step_6_membership_period_stats AS
+                SELECT
+                    id_membership_periods_sa,
+                    actual_membership_fee_6_rule_sa,
+                        
+                    COUNT(id_membership_periods_sa) AS sales_units,
+                    SUM(actual_membership_fee_6_sa) AS sales_revenue
 
-            FROM all_membership_sales_data_2015_left
-            GROUP BY id_membership_periods_sa, actual_membership_fee_6_rule_sa
-        ;
+                --  FROM all_membership_sales_data_2015_left
+                ${FROM_STATEMENT}
+                
+                GROUP BY id_membership_periods_sa, actual_membership_fee_6_rule_sa
+            ;
         
         ALTER TABLE step_6_membership_period_stats              
             ADD INDEX (id_membership_periods_sa);
