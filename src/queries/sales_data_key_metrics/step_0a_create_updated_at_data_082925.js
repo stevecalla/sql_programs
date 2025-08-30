a7fc1b86ecea764db196c5a0d8f1e00a34726626
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-08-30 11:33:19 -0600

    Update get updated at or partial data

--- src/queries/sales_data_key_metrics/step_0a_create_updated_at_data_082925.js
+++ src/queries/sales_data_key_metrics/step_0a_create_updated_at_data_082925.js
@@ -1,41 +1,63 @@
 // SOURCE?
 // C:\Users\calla\development\usat\sql_code\6_create_key_stats\key_stats_query_cte_create_table_100524.sql
 
-function step_0a_created_updated_at_data() {
+function step_0a_create_updated_at_data(FROM_STATEMENT, pool, update_mode, options) {
+    
+    let { TABLE_NAME, membership_period_ends, start_year_mtn, start_date_mtn, end_date_mtn, updated_at_date_mtn } = options;
+
+    const WHERE_STATEMENT = 
+        (update_mode === 'partial') ? `
+                AND starts_mp >= '${start_date_mtn}'
+                AND ends_mp <= '${end_date_mtn}'
+        ` : `
+                AND (
+                    updated_at_mp           >= '${updated_at_date_mtn}'
+                    OR updated_at_members   >= '${updated_at_date_mtn}'
+                    OR updated_at_profiles  >= '${updated_at_date_mtn}'
+                )
+        `;
+
     return `
         -- STEP #0a = CREATE UPDATED AT DATA TABLE
-        DROP TABLE IF EXISTS step_0a_created_updated_at_data;
+        DROP TABLE IF EXISTS step_0a_create_updated_at_data;
 
-            CREATE TABLE step_0a_created_updated_at_data AS
+            CREATE TABLE step_0a_create_updated_at_data AS
                 WITH updated_profiles AS (
-                    -- Finds the set of profile IDs that were touched since the where date indicated
-                    SELECT DISTINCT id_profiles
-                    FROM all_membership_sales_data_2015_left
+
+                    -- FINDS THE SET OF PROFILE IDS BASED ON THE WHERE STATEMENT
+                    SELECT DISTINCT 
+                        id_profiles
+                    FROM ${FROM_STATEMENT}
                     WHERE 1 = 1
-                        AND (
-                            updated_at_mp           >= CURDATE() - INTERVAL 1 DAY
-                            OR updated_at_members   >= CURDATE() - INTERVAL 1 DAY
-                            OR updated_at_profiles  >= CURDATE() - INTERVAL 1 DAY
-                        )
+                        ${WHERE_STATEMENT}
+                        -- AND (
+                        --     updated_at_mp            >= CURDATE() - INTERVAL 1 DAY
+                        --     OR updated_at_members    >= CURDATE() - INTERVAL 1 DAY
+                        --     OR updated_at_profiles   >= CURDATE() - INTERVAL 1 DAY
+                        -- )
                 )
-                    -- Finds all records with id_profile from updated_profiles cte
+
+                    -- FINDS ALL RECORDS WITH id_profiles FROM updated_profiles CTE
                     , all_joined_profile_records AS (
-                        SELECT t.*
-                        FROM all_membership_sales_data_2015_left AS t
+                        SELECT 
+                            t.*
+                        FROM ${FROM_STATEMENT} AS t
                             JOIN updated_profiles AS up USING (id_profiles)
                     )
+
                     SELECT * FROM all_joined_profile_records ORDER BY id_profiles, id_membership_periods_sa
+
                     -- SELECT
                     --   (SELECT FORMAT(COUNT(*), 0) FROM updated_profiles)                   		AS total_updated_profiles,
                     --   (SELECT FORMAT(COUNT(*), 0) FROM all_joined_profile_records)             	AS total_joined_rows,
                     --   (SELECT FORMAT(COUNT(*), 0) FROM all_membership_sales_data_2015_left) 	AS total_all_records                
             ;
 
-            ALTER TABLE step_0a_created_updated_at_data ADD INDEX (id_profiles);     
+            ALTER TABLE step_0a_create_updated_at_data ADD INDEX (id_profiles);     
         -- *********************************************
     `;
 }
 
 module.exports = {
-    step_0a_created_updated_at_data,
+    step_0a_create_updated_at_data,
 }
