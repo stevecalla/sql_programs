0f2e21d64c649b516d7d1a9e02da0625720d42cb
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-08-31 06:50:18 -0600

    Remove pool

--- src/queries/sales_data_key_metrics/step_8_get_sales_data_010425.js
+++ src/queries/sales_data_key_metrics/step_8_get_sales_data_010425.js
@@ -1,7 +1,7 @@
 const { step_8_sales_key_stats_2015_query } = require('./step_8a_get_sales_data_082925_query');
 const { step_8a_sales_key_stats_2015_upsert } = require('./step_8a_get_sales_data_082925_upsert');
 
-async function step_8_sales_key_stats_2015(FROM_STATEMENT, pool) {
+async function step_8_sales_key_stats_2015(FROM_STATEMENT) {
     const query = `
         DROP TABLE IF EXISTS sales_key_stats_2015;
 
