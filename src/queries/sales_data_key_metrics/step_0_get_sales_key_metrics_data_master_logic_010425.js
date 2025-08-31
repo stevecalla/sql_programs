6e6bcb57842f01b3ce2f219c4062c6acd84b647a
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-08-31 06:09:13 -0600

    Update results

--- src/queries/sales_data_key_metrics/step_0_get_sales_key_metrics_data_master_logic_010425.js
+++ src/queries/sales_data_key_metrics/step_0_get_sales_key_metrics_data_master_logic_010425.js
@@ -10,10 +10,9 @@ const { step_6_membership_period_stats } = require('./step_6_get_sales_data_0104
 const { step_7_prior_purchase } = require('./step_7_get_sales_data_010425'); // step 7
 
 const { step_8_sales_key_stats_2015, step_8_sales_key_stats_2015_upsert } = require('./step_8_get_sales_data_010425'); // step 8
-
 const { step_8b_create_indexes } = require('./step_8b_get_sales_data_010425_indexes'); // step 8a
 
-function step_noop() { return `DO 0`; }; // empty sql statement to not run when update_at
+function step_noop() { return `DO 0`; }; // empty sql statement to provide dummy query
 
 async function query_step_0_sales_key_metrics_master_logic(update_mode) {
 
@@ -23,18 +22,19 @@ async function query_step_0_sales_key_metrics_master_logic(update_mode) {
         // #0a doesn't need to run on full table replacement; added 8/29/25
         update_mode === 'full' ? step_noop                      : step_0a_create_updated_at_data,  
 
-        step_1_member_minimum_first_created_at_dates,            // #1 done 2:56, 1,621,815
-        step_2_member_min_created_at_date,                       // #2 done 1:10, 1,621,815
-        step_3_member_total_life_time_purchases,                 // #3 1:32, 1,621,815
-        step_4_member_age_dimensions,                            // #4 2:37, 1,621,815
-        step_5_member_age_at_sale_date,                          // #5 3:17, 3,649,353
-        step_5a_member_age_at_end_of_year_of_sale,               // #6 3:10, 3,649,353
-        step_6_membership_period_stats,                          // #7 6:48, 3649353
-        step_7_prior_purchase,                                   // #8 00:17:00... Query results: 3,649,353
+        step_1_member_minimum_first_created_at_dates,            // #1 Query results: 1664164, Elapsed Time: 332.76 sec
+        step_2_member_min_created_at_date,                       // #2 Query results: 1664164, Elapsed Time: 88.19 sec
+        step_3_member_total_life_time_purchases,                 // #3 Query results: 1664164, Elapsed Time: 120.10 sec
+        step_4_member_age_dimensions,                            // #4 Query results: 1664164, Elapsed Time: 142.50 sec
+        step_5_member_age_at_sale_date,                          // #5 Query results: 3831769, Elapsed Time: 95.02 sec
+        step_5a_member_age_at_end_of_year_of_sale,               // #5a Query results: 3831769, Elapsed Time: 98.80 sec
+        step_6_membership_period_stats,                          // #6 Query results: 3831769, Elapsed Time: 349.92 sec
 
-        // #9 / #10 on full drop & create table; on partial or updated_at upsert
-        update_mode === 'full' ? step_8_sales_key_stats_2015    : step_8_sales_key_stats_2015_upsert,   // #9
-        update_mode === 'full' ? step_8b_create_indexes         : step_noop,                            //#10
+        step_7_prior_purchase,                                   // #7 Query results: undefined, Elapsed Time: 840.97 sec
+
+        // #8 / #8b on full drop & create table; on partial or updated_at upsert
+        update_mode === 'full' ? step_8_sales_key_stats_2015    : step_8_sales_key_stats_2015_upsert,   // #8 Query results: 3831769, Elapsed Time: 2560.93 sec
+        update_mode === 'full' ? step_8b_create_indexes         : step_noop,                            // #8b Query results: undefined, Elapsed Time: 2483.21 sec
     ]
     return query_list;
 }
