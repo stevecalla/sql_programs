3762a3475c991402512d8dee3122aac66d19ff9e
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-08-31 07:04:53 -0600

    Initial version

--- src/queries/sales_data_key_metrics/step_8_get_sales_data_010425_rebuild.js
+++ src/queries/sales_data_key_metrics/step_8_get_sales_data_010425_rebuild.js
@@ -1,18 +1,41 @@
 const { step_8_sales_key_stats_2015_query } = require('./step_8a_get_sales_data_082925_query');
 const { step_8a_sales_key_stats_2015_upsert } = require('./step_8a_get_sales_data_082925_upsert');
 
-async function step_8_sales_key_stats_2015(FROM_STATEMENT, pool) {
-    const query = `
-        DROP TABLE IF EXISTS sales_key_stats_2015;
+const table_name = `sales_key_stats_2015_test`;
 
-            CREATE TABLE sales_key_stats_2015 AS
-                ${await step_8_sales_key_stats_2015_query(FROM_STATEMENT)}
-            ;
+async function step_8_sales_key_stats_2015_test(FROM_STATEMENT) {
+    const query = `
+
+        -- Drop current table (if any)
+        DROP TABLE IF EXISTS ${table_name};
+
+        -- Compute fixed timestamps once
+        SET @now_utc := NOW();
+        SET @dt_mtn  := DATE_FORMAT(DATE_ADD(@now_utc, INTERVAL -6 HOUR), '%Y-%m-%d');
+        SET @dt_utc  := DATE_FORMAT(@now_utc, '%Y-%m-%d');
+
+        -- Create table structure by selecting 0 rows (captures all columns & types)
+        CREATE TABLE ${table_name}
+            ENGINE=InnoDB
+            AS
+            -- SELECT
+
+            ${await step_8_sales_key_stats_2015_query(FROM_STATEMENT)}
+
+            WHERE 1=0
+        ;
+
+        -- Add NOT NULL + PK (cheap now because the table is empty)
+        ALTER TABLE ${table_name}
+            MODIFY id_profiles BIGINT NOT NULL,
+            MODIFY id_membership_periods_sa BIGINT NOT NULL,
+            ADD PRIMARY KEY (id_profiles, id_membership_periods_sa);
         `
     ;
+
     return query;
 }
 
 module.exports = {
-    step_8_sales_key_stats_2015,
+    step_8_sales_key_stats_2015_test,
 }
