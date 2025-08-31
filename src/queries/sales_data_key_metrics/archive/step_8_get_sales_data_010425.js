e74507ec9b20a1b069006d6b1e9e91725f0aff54
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-08-31 17:26:02 -0600

    Create archive

---
+++ src/queries/sales_data_key_metrics/archive/step_8_get_sales_data_010425.js
@@ -0,0 +1,27 @@
+// const { step_8_sales_key_stats_2015_query } = require('./step_8a_get_sales_data_082925_query');
+// const { step_8a_sales_key_stats_2015_upsert } = require('./step_8a_get_sales_data_082925_upsert');
+
+// async function step_8_sales_key_stats_2015(FROM_STATEMENT, pool, update_mode, options) {
+
+//     const query = `
+//         DROP TABLE IF EXISTS ${options.TARGET_TABLE_NAME};
+
+//             CREATE TABLE ${options.TARGET_TABLE_NAME} AS
+//                 ${await step_8_sales_key_stats_2015_query(FROM_STATEMENT)}
+//             ;
+//         `
+//     ;
+//     return query;
+// }
+
+// async function step_8_sales_key_stats_2015_upsert(FROM_STATEMENT, pool, update_mode, options) {
+
+//     const query = await step_8a_sales_key_stats_2015_upsert(FROM_STATEMENT, pool, update_mode, options);
+
+//     return query;
+// }
+
+// module.exports = {
+//     step_8_sales_key_stats_2015,
+//     step_8_sales_key_stats_2015_upsert,
+// }
