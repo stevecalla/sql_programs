0a0b716acf5e91f9908cea2771b5577a135b91c1
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2026-04-21 13:11:54 -0600

    Add steps to load bq

---
+++ src/scraper_runsignup_api/step_5_load_bq_runsignup_affiliate_url.js
@@ -0,0 +1,59 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: "../../.env" });
+
+const { logPM2MemoryUsage } = require('../../utilities/pm2_scripts/pm2_log_memory_usage');
+const { runTimer, stopTimer } = require('../../utilities/timer');
+
+const { query_runsignup_affiliate_data } = require('../google_cloud/queries/query_runsignup_affiliate_data');
+const { all_runsignup_affiliate_match_schema } = require('../google_cloud/schemas/schema_runsignup_affiliate_datajs');
+
+const { execute_load_data_to_bigquery } = require('../google_cloud/step_0_load_main_job');
+
+
+// EXECUTE LOAD BIQ QUERY
+async function main() {
+    runTimer(`load_bigquery`);
+
+    const app_name = "usat_sales";
+    await logPM2MemoryUsage(app_name);
+
+    const options = [
+        {
+            fileName: 'runsignup_data_raw_affiliate_urls',
+            query: (retrieval_batch_size, offset) => query_runsignup_affiliate_data(retrieval_batch_size, offset, 'all_runsignup_data_raw_affiliate_urls'),
+            tableId: 'runsignup_data_raw_affiliate_urls', // table name
+
+            // fileName: 'runsignup_data_raw_affiliate_urls_v2',
+            // tableId: "runsignup_data_raw_affiliate_urls_v2",
+        }
+    ];
+
+    const directoryName = `usat_bigquery_${options[0].fileName}`;
+    const datasetId = "membership_reporting"; // database name
+    const bucketName = 'membership-reporting';
+    const schema = all_runsignup_affiliate_match_schema;
+
+    await execute_load_data_to_bigquery(options, datasetId, bucketName, schema, directoryName);
+
+    // the process to get data for biqquery loads a lot data in step 1 results
+    // within that process, results are assigned to null then garbage collection is run 
+    // to clear memory
+    await logPM2MemoryUsage(app_name);
+
+    stopTimer(`load_bigquery`);
+
+    return true; // placeholder to return to ensure success msg
+}
+
+if (require.main === module) {
+    try {
+        console.log('\nStarting data load.');
+        main();
+    } catch (error) {
+        console.error("Error during data load:", error);
+    }
+}
+
+module.exports = {
+    execute_load_big_query_runsignup_affliate_url: main,
+}
